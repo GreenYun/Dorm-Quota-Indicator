e10456eeb902705710663150eecc672dbd134d52
Gamcheong Yuen <microsoft.zh.cn@gmail.com>
2019-06-23 00:53:31 +0800

    Initial Commit

---
+++ Dorm Quota Indicator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dorm Quota Indicator
+//
+//  Created by Gamcheong Yuen on 23/6/2019.
+//  Copyright © 2019 GreenYun Organization. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dorm Quota IndicatorTests/Dorm_Quota_IndicatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dorm_Quota_IndicatorTests.swift
+//  Dorm Quota IndicatorTests
+//
+//  Created by Gamcheong Yuen on 23/6/2019.
+//  Copyright © 2019 GreenYun Organization. All rights reserved.
+//
+
+import XCTest
+@testable import Dorm_Quota_Indicator
+
+class Dorm_Quota_IndicatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
